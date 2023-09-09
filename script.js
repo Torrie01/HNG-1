d3f4fcf5eb9e4cd5234c662f6fd5eec7e0e6efe8
Torrie01 <101416134+Torrie01@users.noreply.github.com>
2023-09-09 13:41:12 +0100

    my first task on HNG

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     // Get the current day of the week
     const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const currentDate = new Date();
     const currentDayOfWeek = daysOfWeek[currentDate.getUTCDay()];
-
+  
     // Get current UTC time (hours, minutes, and seconds)
-    const currentUTCHours = currentDate.getUTCHours();
-    const currentUTCMinutes = currentDate.getUTCMinutes();
-    const currentUTCSeconds = currentDate.getUTCSeconds();
-
+    let currentUTCHours = currentDate.getUTCHours();
+    currentUTCHours = currentUTCHours < 10 ? `0${currentUTCHours}` : currentUTCHours;
+    let currentUTCMinutes = currentDate.getUTCMinutes();
+    currentUTCMinutes = currentUTCMinutes < 10 ? `0${currentUTCMinutes}` : currentUTCMinutes;
+    let currentUTCSeconds = currentDate.getUTCSeconds();
+    currentUTCSeconds = currentUTCSeconds < 10 ? `0${currentUTCSeconds}` : currentUTCSeconds;
+  
     // Display current day of the week and UTC time
-    const dayOfWeekElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
-    dayOfWeekElement.textContent = `Current Day: ${currentDayOfWeek}`;
-
-    const utcTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
-    utcTimeElement.textContent = `Current UTC Time: ${currentUTCHours}:${currentUTCMinutes}:${currentUTCSeconds}`;
-});
+    const dayOfWeekElement = document.querySelector(
+      '[data-testid="currentDayOfTheWeek"]'
+    );
+  
+    dayOfWeekElement.textContent = `${currentDayOfWeek}`;
+  
+    const utcTimeElement = document.querySelector(
+      '[data-testid="currentUTCTime"]'
+    );
+    utcTimeElement.textContent = `${currentUTCHours}:${currentUTCMinutes}:${currentUTCSeconds}`;
+  });
