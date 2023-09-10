194f76d92ada2e8f3b56ccb146dda99ec0c0fd3e
Torrie01 <101416134+Torrie01@users.noreply.github.com>
2023-09-10 14:44:39 +0100

    My first task on HNG

--- script.js
+++ script.js
@@ -11,13 +11,17 @@ document.addEventListener("DOMContentLoaded", function() {
     const currentUTCMilliseconds = currentDate.getUTCMilliseconds();
 
   // Format minutes with leading zeros
-    const UTCMinutes = currentUTCMinutes.toString().padStart(2, '0');
+    const formattedUTCMinutes = currentUTCMinutes.toString().padStart(2, '0');
+
+  // Format milliseconds with leading zeros and three decimal places
+    const formattedUTCMilliseconds = currentUTCMilliseconds.toString().padStart(3, '0');
 
   // Display current day of the week
     const dayOfWeekElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
-    dayOfWeekElement.textContent = `Current Day of the week: ${currentDayOfWeek}`;
+    dayOfWeekElement.textContent = `Current Day: ${currentDayOfWeek}`;
 
   // Display current UTC time with formatted minutes and milliseconds
     const utcTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
-    utcTimeElement.textContent = `UTC Time: ${currentUTCHours}:${UTCMinutes}:${currentUTCSeconds}.${currentUTCMilliseconds}`;
+    utcTimeElement.textContent = `Current UTC Time: ${currentUTCHours}:${formattedUTCMinutes}:${currentUTCSeconds}.${formattedUTCMilliseconds}`;
 });
+
